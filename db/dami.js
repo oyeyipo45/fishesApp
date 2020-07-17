d3a67cade62fd2a6bb5e93853a8a7e083be68ca5
oyeyipo45 <oyeyipo45@gmail.com>
2020-07-17 13:54:55 +0100

    deleted submodule in client folder

---
+++ db/dami.js
@@ -0,0 +1,14 @@
+test('generates a unique number', () => {
+    const id = getRandomId();
+    const index = currentIds.indexOf(id);
+    expect(index).toBe(-1);
+ });test('generates a unique number', () => {
+    const id = getRandomId();
+    const index = currentIds.indexOf(id);
+    expect(index).toBe(-1);
+ });test('generates a unique number', () => {
+    const id = getRandomId();
+    const index = currentIds.indexOf(id);
+    expect(index).toBe(-1);
+ });
+ 
